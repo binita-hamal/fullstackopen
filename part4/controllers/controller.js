5b9786abf265685ad54e00fb6ef2322d90ad7a03
winiiee <binita.hamal7@gmail.com>
2025-09-07 14:16:05 +0545

    4.21 exercise completed

--- part4/controllers/controller.js
+++ part4/controllers/controller.js
@@ -1,5 +1,6 @@
 import { Blog } from "../models/blog.js";
 import { User } from "../models/user.js";
+import jwt, { decode } from "jsonwebtoken"
 
 export const getBlogs = async (req, res) => {
   try {
@@ -24,7 +25,7 @@ export const getBlogs = async (req, res) => {
 
 export const postBlogs = async (req, res) => {
   try {
-    // const { title, author, url, likes } = req.body;
+    const { title, author, url, likes } = req.body;
     const user = req.user;
 
 
@@ -54,17 +55,58 @@ export const postBlogs = async (req, res) => {
 };
 
 export const deleteBlogs = async (req, res) => {
-  try {
-    const id = req.params.bid;
-    const deleteBlog = await Blog.findByIdAndDelete(id);
 
-    if (!deleteBlog) {
-      return res.status(404).json({ error: "Blog not found" });
+  const authorization =req.get("authorization")
+    let token = null
+    if(authorization && authorization.toLowerCase().startsWith("bearer ")){
+      token = authorization.substring(7)
     }
-    res.status(204).end();
+
+    if(!token){
+      return res.status(401).json({error:"token missing or invalid"})
+    }
+    let decodedToken
+  try {
+    // const id = req.params.bid;
+    // const deleteBlog = await Blog.findByIdAndDelete(id);
+
+    // if (!deleteBlog) {
+    //   return res.status(404).json({ error: "Blog not found" });
+    // }
+    // res.status(204).end();
+
+    decodedToken  = jwt.verify(token,process.env.mySECRET)
+
+
+    
+
+
+
   } catch (error) {
     res.status(500).json({ error: "something went wrong" });
   }
+
+  if(!decodedToken.id){
+    return res.status(401).json({error:"token missing or invalid"})
+  }
+
+  const blog = await Blog.findById(req.params.id)
+
+  if(!blog){
+    return res.status(404).json({error:"blog not found"})
+  }
+
+  if(blog.user.toString() !== decodedToken.id.toString()){
+    return res.status(403).json({error:"only creater can delete the blog"})
+  }
+
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
+
+
+
+
+
 };
 
 export const updateBlogs = async (req, res) => {
