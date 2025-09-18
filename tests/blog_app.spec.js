af427dfebbbbf7d04045cb1d93e2b535d08bc84a
winiiee <binita.hamal7@gmail.com>
2025-09-18 08:55:06 +0545

    5.19 exercise completed

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -51,4 +51,48 @@ describe("Blog app", () => {
       await expect(page.getByText(/bini logged in/i)).not.toBeVisible();
     });
   });
+
+
+
+
+  describe("when logged in", ()=>{
+
+    beforeEach(async({page})=>{
+        await page.getByRole("button", {name:"log in"}).click()
+        await page.getByLabel(/username/i).fill("bini")
+        await page.getByLabel(/password/i).fill("password")
+        await page.getByRole("button", { name: /^login$/i }).click();
+
+
+        await expect(page.getByText(/bini logged in/i)).not.toBeVisible();
+        
+
+    })
+
+    test("a blog can be created", async({page})=>{
+        await page.getByRole("button", {name:/create new blog/i}).click()
+
+        await page.getByLabel(/title/i).fill("kite runner")
+        await page.getByLabel(/author/i).fill("khaled")
+        await page.getByLabel(/url/i).fill("google.com")
+
+
+        await page.getByRole("button", {name:/create/i}).click()
+
+        //visible
+        const blogList = page.locator('.blog-summary')
+        await expect(blogList.last().getByText("kite runner")).toBeVisible()
+        await expect(blogList.last().getByText("khaled")).toBeVisible()
+
+
+
+
+    })
+
+
+  })
+
+
+
+
 });
